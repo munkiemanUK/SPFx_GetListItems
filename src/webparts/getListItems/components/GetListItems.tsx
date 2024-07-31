e24f13d1431dd40982478656b567f5b877503492
Jason Clark <jclark@maximusuk.co.uk>
2024-07-31 17:47:13 +0100

    trying different options to display html

--- src/webparts/getListItems/components/GetListItems.tsx
+++ src/webparts/getListItems/components/GetListItems.tsx
@@ -3,42 +3,23 @@ import styles from './GetListItems.module.scss';
 import type { IGetListItemsProps } from './IGetListItemsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
-// import interfaces
-//import { IFile, IResponseItem } from "./interfaces";
-
 import { SPFI } from "@pnp/sp";
-//import { Caching } from "@pnp/queryable";
-//import { Logger, LogLevel } from "@pnp/logging";
 import "@pnp/sp/webs";
 import "@pnp/sp/lists";
-//import "@pnp/sp/items";
+import "@pnp/sp/items";
 import { getSP } from '../pnpjsConfig';
 
-//import {
-//  SPHttpClient,
-//  SPHttpClientResponse   
-//} from '@microsoft/sp-http';
-
-//export interface ISPLists {
-//  value: ISPList[];
-//}
-
-//export interface ISPList {
-//  Title: string;
-//}
-
-let panelHTML: string;
-
+//let panelHTML: string;
 export interface IAsyncAwaitPnPJsProps {
   description: string;
 }
 
-export interface IIPnPjsExampleState {
-  //items: IFile[];
-  errors: string[];
+export interface IState {  
+  listItems: any;
+  listFlag: boolean;
 }
 
-export default class GetListItems extends React.Component<IGetListItemsProps, {}> {
+export default class GetListItems extends React.Component<IGetListItemsProps,IState,{}> {
   //constructor(props: IGetListItemsProps | Readonly<IGetListItemsProps>) {
   //  super(props);
   //}
@@ -49,10 +30,11 @@ export default class GetListItems extends React.Component<IGetListItemsProps, {}
 
   constructor(props: IGetListItemsProps) {
     super(props);
+
     // set initial state
     this.state = {
-      items: [],
-      errors: []
+      listFlag: false,
+      listItems: [],
     };
     this._sp = getSP();
   }
@@ -83,7 +65,7 @@ export default class GetListItems extends React.Component<IGetListItemsProps, {}
           <div>Web part property value: <strong>{escape(description)}</strong></div>
         </div>
         <h4>List Items</h4>
-        {panelHTML}
+        {this.state.listItems}
         <div>
           <h3>Welcome to SharePoint Framework!</h3>
           <p>
@@ -114,11 +96,14 @@ export default class GetListItems extends React.Component<IGetListItemsProps, {}
 
   private async _renderListAsync(): Promise<void> { 
     const items : any[] = await this._sp.web.lists.getByTitle('Important Links').items();
+    let htmlString : string;
+
     console.log("items",items);
     items.forEach((item) => {
       console.log(item.LinkName);
       //const linkTitle = item.LinkName;
-      this.setState({items:`<div>${item.LinkName}</div>`});            
+      htmlString += `<div>${item.LinkName}</div>`;  
+      this.setState({listItems:htmlString})           
     });
   }
 }
